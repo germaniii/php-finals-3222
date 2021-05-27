9b504b0bd4bfac26c18343b685b47bb04ebc74f0
German III <germaniiifelisarta@gmail.com>
2021-05-28 04:01:10 +0800

    v0.3 Added CSS and Initial Design

--- index.php
+++ index.php
@@ -2,73 +2,84 @@
 <html>
 <head>
 	<title>Address Book</title>
-	<link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" type="text/css" href="styles.css">
+	<meta http-equiv="Cache-control" content="no-cache">
 </head>
 <body>
-	<h1>Contact Tracer</h1>	
-	<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-			
-		ID Number: <input type="text" name="id"><br><br>	
-		<input type="submit" name="signin" value="sign in"><br><br><br>	
-		Click here to register: <br><br>
-		<input type="submit" name="register" value="register"><br><br>
-		<input type="submit" name="dispall" value="display all contacts"><br><br>
+	<div class="header">
+		<h1>Contact Tracer</h1>	
+    </div>
 
-	</form>
-	<h1>Log Book</h1>	
-
-	<?php
-		date_default_timezone_set('Asia/Manila');
-		error_reporting (E_ALL ^ E_NOTICE);	//remove notices
-		include_once 'db.inc.php';		//database reference
+	<div class="content">
+		<h2>Sign In/Out</h2>
+		<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+			ID Number: <input type="tel" pattern="[0-9]{8}" placeholder="16101002"name="id" required><br><br>	
+			<input type="submit" name="signin" value="sign in"><br>
+		</form>
+		<h2>Register</h2>
 		
-		if(isset($_POST['dispall'])){		//display all
-			$sql = "SELECT * from contact";		//SQL statement
-			$result = mysqli_query($conn, $sql);		//send SQL statement to database
-			$resultcheck = mysqli_num_rows($result);	//check if there is a result
+		<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+			<input class="button" type="submit" name="register" value="register">
+			<input class="button" type="submit" name="dispall" value="display all contacts">
+		</form>
+	</div>
+
+	<div class="content">
+		<h2>Log Book</h2>
+		<?php
+			date_default_timezone_set('Asia/Manila');
+			error_reporting (E_ALL ^ E_NOTICE);	//remove notices
+			include_once 'db.inc.php';		//database reference
+			
+			if(isset($_POST['dispall'])){		//display all
+				$sql = "SELECT * from contact";		//SQL statement
+				$result = mysqli_query($conn, $sql);		//send SQL statement to database
+				$resultcheck = mysqli_num_rows($result);	//check if there is a result
+					if($resultcheck > 0){
+						while($row = mysqli_fetch_assoc($result)){
+							
+							echo $row['id'], ", ";
+							echo $row['name'], ", ";
+							echo $row['address'], ", ";
+							echo $row['number'], ", ";
+							echo $row['email'], ", ";
+							echo $row['date'], ", ";	
+							echo "<br><br>";
+						}
+					}
+					else if($resultcheck == 0 || $resultcheck == FALSE){			//if no contact in database display no result
+						echo "No results";
+					}
+			}
+
+			else if(isset($_POST['register'])){		//go to register.php
+				header("Location: register.php");
+			}
+			
+			else if(isset($_POST['signin'])){
+				$id = $_POST['id'];
+				$sql = "select * from contact where id = '$id'";		
+				$result = mysqli_query($conn, $sql);		//send SQL statement to database
+				$resultcheck = mysqli_num_rows($result);	//check if there is a result
 				if($resultcheck > 0){
 					while($row = mysqli_fetch_assoc($result)){
 						
-						echo $row['id'], ", ";
-						echo $row['name'], ", ";
-						echo $row['address'], ", ";
-						echo $row['number'], ", ";
-						echo $row['email'], ", ";
-						echo $row['date'], ", ";	
-						echo "<br><br>";
+						
+						$date = date("d-m-y, h:i:sa");
+					
+						$reg = "update contact set date='$date' where id='$id';";
+						mysqli_query($conn, $reg);
+						echo $row['id'], ", logged in at ", $date;
 					}
 				}
-				else if($resultcheck == 0 || $resultcheck == FALSE){			//if no contact in database display no result
-					echo "No results";
+				else if($resultcheck == 0){			//if no contact in database display no result
+					echo "ID number not registered";
 				}
-		}
+			}
+			
+		?>
 
-		else if(isset($_POST['register'])){		//go to register.php
-			header("Location: register.php");
-		}
-		
-		else if(isset($_POST['signin'])){
-			$id = $_POST['id'];
-			$sql = "select * from contact where id = '$id'";		
-			$result = mysqli_query($conn, $sql);		//send SQL statement to database
-			$resultcheck = mysqli_num_rows($result);	//check if there is a result
-			if($resultcheck > 0){
-				while($row = mysqli_fetch_assoc($result)){
-					
-					
-					$date = date("d-m-y, h:i:sa");
-				
-					$reg = "update contact set date='$date' where id='$id';";
-					mysqli_query($conn, $reg);
-					echo $row['id'], ", logged in at ", $date;
-				}
-			}
-			else if($resultcheck == 0){			//if no contact in database display no result
-				echo "ID number not registered";
-			}
-		}
-		
-	?>
+	</div>
 
 	
 </body>
--- register.php
+++ register.php
@@ -2,21 +2,30 @@
 <html>
 <head>
 	<title>Address Book</title>
-	<link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" type="text/css" href="styles.css">
+	<meta http-equiv="Cache-control" content="no-cache">
 </head>
 <body>
-	
-	<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-		<h1>Register Form</h1>		
-		ID Number: <input type="text" name="id"><br><br>
-		Name: <input type="text" name="name"><br><br>
-		Address: <input type="text" name="address"><br><br>
-		Number: <input type="text" name="number"><br><br>
-		Email: <input type="text" name="email"><br><br>
+	<div class="header">
+		<h1>Contact Tracer</h1>	
+    </div>
 
-		<input type="submit" name="submit" value="submit"><br><br>
-		<input type="submit" name="back" value="back"><br><br>
-	</form>	
+	<div class="content">
+		<h2>Register Form</h2>	
+		<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">	
+			<input size="50" type="tel" pattern="[0-9]{8}" placeholder="ID Number (16101002)" name="id" required><br><br>
+			<input size="50" type="text" placeholder="Name (Juan Dela Cruz)" name="name" required><br><br>
+			<input size="50" type="text" placeholder="Address (Purok Karne, San Isidro, Lucena City, Cebu)" name="address" required><br><br>
+			<input size="50" type="tel" pattern="[0-9]{11}" placeholder="Number (09123456789)" name="number" required><br><br>
+			<input size="50" type="email" placeholder="USC Email (xyz@usc.edu.ph)" name="email" required><br><br>
+			<input type="submit" name="submit" value="submit"><br><br>
+		</form>	
+
+		
+		<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">	
+			<input type="submit" name="back" value="back"><br><br>
+		</form>	
+	</div>
 
 
 	<?php
@@ -27,16 +36,16 @@
 		
 		
 		if(isset($_POST['submit'])){
-		$id = $_POST['id'];
-        	$name = $_POST['name'];
-		$address = $_POST['address'];
-        	$number = $_POST['number'];
-		$email = $_POST['email'];
-		$date = date("d-m-y, h:i:sa");
-        	
-		$reg = "insert into contact (id, name, address, number, email, date) VALUES ('$id', '$name', '$address', '$number', '$email', '$date');";
-		mysqli_query($conn, $reg);
-		echo "Logbook updated";
+			$id = $_POST['id'];
+				$name = $_POST['name'];
+			$address = $_POST['address'];
+				$number = $_POST['number'];
+			$email = $_POST['email'];
+			$date = date("d-m-y, h:i:sa");
+				
+			$reg = "insert into contact (id, name, address, number, email, date) VALUES ('$id', '$name', '$address', '$number', '$email', '$date');";
+			mysqli_query($conn, $reg);
+			echo "Logbook updated";
 		}
 		else if(isset($_POST['back'])){
 			header("Location: index.php");
