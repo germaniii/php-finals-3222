a908b6e79ce13d63c6054fd37c42934b2fd0aea1
German III <germaniiifelisarta@gmail.com>
2021-05-28 16:23:56 +0800

    v0.6 Updated Design of the Website

--- admin.php
+++ admin.php
@@ -13,46 +13,54 @@
         </ul>
     </div>
 
-	<div class="content">
-		<h2>Search City</h2>
-		<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-			<input type="text" placeholder="Lucena City" name="search_city" required>	
-			<input type="submit" name="search_city_button" value="Search City"><br>
-		</form>
-        <h2>Search Barangay</h2>
-		<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-			<input type="text" placeholder="San Isidro" name="search_brgy" required>	
-			<input type="submit" name="search_brgy_button" value="Search Brgy"><br>
-		</form>
-        <h2>Search Province</h2>
-		<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-			<input type="text" placeholder="Cebu" name="search_prov" required>
-			<input type="submit" name="search_prov_button" value="Search Province"><br>
-		</form>
-        <h2>Search ID Number</h2>
-		<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-			<input type="tel" pattern="[0-9]{8}" placeholder="16101002" name="search_id" required>	
-			<input type="submit" name="search_id_button" value="Search ID Number"><br>
-		</form>
-        <h2>Search Name</h2>
-		<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-			<input type="text" placeholder="Juan Dela Cruz" name="search_name" required>	
-			<input type="submit" name="search_name_button" value="Search Name"><br>
-		</form>
-        <h2>Search Time and Day</h2>
-		<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-			<input type="datetime-local" name="search_time" required>
-			<input type="submit" name="search_time_button" value="Search Time and Day"><br>
-		</form>
+	<div class="content-search">
+        <div class="searchtable">
+            <div class="searchtable-row">
+                <div class="left">
+                    <h2>Search City</h2>
+                    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+                        <input type="text" placeholder="Lucena City" name="search_city" required>	
+                        <input type="submit" name="search_city_button" value="Search City"><br>
+                    </form>
+                    <h2>Search Barangay</h2>
+                    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+                        <input type="text" placeholder="San Isidro" name="search_brgy" required>	
+                        <input type="submit" name="search_brgy_button" value="Search Brgy"><br>
+                    </form>
+                    <h2>Search Province</h2>
+                    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+                        <input type="text" placeholder="Cebu" name="search_prov" required>
+                        <input type="submit" name="search_prov_button" value="Search Province"><br>
+                    </form>
+                    <h2>Search ID Number</h2>
+                    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+                        <input type="tel" pattern="[0-9]{8}" placeholder="16101002" name="search_id" required>	
+                        <input type="submit" name="search_id_button" value="Search ID Number"><br>
+                    </form>
+                </div>
+                <div class="right">
+                    <h2>Search Name</h2>
+                    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+                        <input type="text" placeholder="Juan Dela Cruz" name="search_name" required>	
+                        <input type="submit" name="search_name_button" value="Search Name"><br>
+                    </form>
+                    <h2>Search Time and Day</h2>
+                    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+                        <input type="datetime-local" name="search_time" required>
+                        <input type="submit" name="search_time_button" value="Search Time and Day"><br>
+                    </form>
 
-		<h2>Display Log Book</h2>
-		<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-			<input class="button" type="submit" name="dispall" value="Display All"><br><br>
-		</form>
+                    <h2>Display Log Book</h2>
+                    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+                        <input class="button" type="submit" name="dispall" value="Display All"><br><br>
+                    </form>
 
-        <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">	
-			<input type="submit" name="back" value="Go Back"><br><br>
-		</form>	
+                    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">	
+                        <input type="submit" name="back" value="Go Back"><br><br>
+                    </form>	
+                </div>
+            </div>
+        </div>
 	</div>
 
 	<div class="content-log">
