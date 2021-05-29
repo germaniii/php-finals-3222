71a8693cc84f37e9b4264eb84fddf95a792a2a27
Jan Carlo Orlanda <70943802+carlitorlando@users.noreply.github.com>
2021-05-29 20:13:17 +0800

    Update admin.php
    -made a function for checking the results then display to table
    -added search for city, province, barangay, id, and lastname.
    -no drop down option for city and barangay yet, only text type search

--- admin.php
+++ admin.php
@@ -19,6 +19,7 @@
                 <div class="left">
                     <h2>Search City</h2>
                     <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+			
                         <input type="text" placeholder="Lucena City" name="search_city" required>	
                         <input type="submit" name="search_city_button" value="Search City"><br>
                     </form>
@@ -41,7 +42,7 @@
                 <div class="right">
                     <h2>Search Name</h2>
                     <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-                        <input type="text" placeholder="Juan Dela Cruz" name="search_name" required>	
+                        <input type="text" placeholder="Surname" name="search_name" required>	
                         <input type="submit" name="search_name_button" value="Search Name"><br>
                     </form>
                     <h2>Search Time and Day</h2>
@@ -64,19 +65,74 @@
 	</div>
 
 	<div class="content-log">
-		<h2>Log Book</h2>
-		<?php
-			date_default_timezone_set('Asia/Manila');
-			error_reporting (E_ALL ^ E_NOTICE);	//remove notices
-			include_once 'db.inc.php';		//database reference
+	<h2>Log Book</h2>
+		
+///////////////////////////////////////////////////   PHP   //////////////////////////////////////////////////////		
+	<?php
+		date_default_timezone_set('Asia/Manila');
+		error_reporting (E_ALL ^ E_NOTICE);	//remove notices
+		include_once 'db.inc.php';		//database reference
 			
-			if(isset($_POST['dispall'])){		//display all
-				$sql = "SELECT * from contact";		//SQL statement
-				$result = mysqli_query($conn, $sql);		//send SQL statement to database
-				$resultcheck = mysqli_num_rows($result);	//check if there is a result
-					if($resultcheck > 0){
-                        echo "<table class=\"table-log\">";
-                        echo "<tr>
+		if(isset($_POST['dispall'])){		//display all
+			$sql = "SELECT * from contact";		//SQL statement
+			$result = mysqli_query($conn, $sql);		//send SQL statement to database
+			writeMsg($result);			//function to check for results then display results
+            	}
+
+            	else if(isset($_POST['search_city_button'])){
+			// city to search
+    			$city = $_POST['search_city'];
+   
+    			// mysql search query
+    			$query = "SELECT * FROM contact WHERE caddress = '$city'";		//sql statement to look for contacts with inputted city
+    			writeMsg($result);
+
+            	}else if(isset($_POST['search_brgy_button'])){
+                	// brgy to search
+    			$brgy = $_POST['search_brgy'];
+   
+    			// mysql search query
+    			$query = "SELECT * FROM contact WHERE baddress = '$brgy'";		//sql statement to look for contacts with inputted brgy
+    			writeMsg($result);
+            	}else if(isset($_POST['search_prov_button'])){
+                	// province to search
+    			$prov = $_POST['search_prov'];
+   
+    			// mysql search query
+    			$query = "SELECT * FROM contact WHERE paddress = '$prov'";		//sql statement to look for contacts with inputted province
+    			writeMsg($result);
+
+            	}else if(isset($_POST['search_id_button'])){
+                	// id to search
+    			$id = $_POST['search_id'];
+   
+    			// mysql search query
+    			$query = "SELECT * FROM contact WHERE id = $id LIMIT 1";		//sql statement to look for contacts with inputted id 
+    			writeMsg($result);							//Limit 1 because id is unique
+
+            	}else if(isset($_POST['search_name_button'])){
+                	// name to search
+    			$name = $_POST['search_name'];
+   
+    			// mysql search query
+    			$query = "SELECT * FROM contact WHERE lname = '$name'";			//sql statement to look for contacts with inputted lastname
+    
+    
+    			$result = mysqli_query($conn, $query);					//send SQL statement to database
+			writeMsg($result);		
+            	}else if(isset($_POST['search_time_button'])){					//work in progress sksksks
+                
+            	}
+
+            	else if(isset($_POST['back'])){
+                	header("Location: index.php");
+            	}
+//-------------------------------  FUNCTION  ------------------------------------------//
+	function writeMsg($result) {			//function to check for results then display results
+ 	$resultcheck = mysqli_num_rows($result);	//check if there is a result
+		if($resultcheck > 0){
+                echo "<table class=\"table-log\">";
+                echo "<tr>
                                 <th>ID
                                 <th>First Name</th>
                                 <th>Last Name</th>
@@ -87,47 +143,30 @@
                                 <th>Email</th>
                                 <th>Sign-In</th>
                                 <th>Sign-Out</th>";
-						while($row = mysqli_fetch_assoc($result)){
-							echo "<tr>";
-							echo "<td>", $row['id'], "</td>";
-							echo "<td>", $row['fname'], "</td>";
-							echo "<td>", $row['lname'], "</td>";
-							echo "<td>", $row['baddress'], "</td>";
-							echo "<td>", $row['caddress'], "</td>";
-							echo "<td>", $row['paddress'], "</td>";
-							echo "<td>", $row['number'], "</td>";
-							echo "<td>", $row['email'], "</td>";
-							echo "<td>", $row['datein'], "</td>";	
-							echo "<td>", $row['dateout'], "</td>";	
-                            echo "</tr>";
-						}
-                        echo "</table>";
+					while($row = mysqli_fetch_assoc($result)){
+						echo "<tr>";
+						echo "<td>", $row['id'], "</td>";
+						echo "<td>", $row['fname'], "</td>";
+						echo "<td>", $row['lname'], "</td>";
+						echo "<td>", $row['baddress'], "</td>";
+						echo "<td>", $row['caddress'], "</td>";
+						echo "<td>", $row['paddress'], "</td>";
+						echo "<td>", $row['number'], "</td>";
+						echo "<td>", $row['email'], "</td>";
+						echo "<td>", $row['datein'], "</td>";
+						echo "<td>", $row['dateout'], "</td>";
+							
+                            			echo "</tr>";
 					}
-					else if($resultcheck == 0 || $resultcheck == FALSE){			//if no contact in database display no result
-						echo "No results";
-					}
-            }
-
-            else if(isset($_POST['search_city_button'])){
-
-            }else if(isset($_POST['search_brgy_button'])){
-                
-            }else if(isset($_POST['search_prov_button'])){
-                
-            }else if(isset($_POST['search_id_button'])){
-                
-            }else if(isset($_POST['search_name_button'])){
-                
-            }else if(isset($_POST['search_time_button'])){
-                
-            }
-
-            else if(isset($_POST['back'])){
-                header("Location: index.php");
-            }
-			
-		?>
-
+                echo "</table>";
+		}
+		else if($resultcheck == 0 || $resultcheck == FALSE){			//if no contact in database display no result
+			echo "No results";
+		}
+	}
+//-------------------------------  FUNCTION  ------------------------------------------//	
+	?>
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
 	</div>
 
 	
